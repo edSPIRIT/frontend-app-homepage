388c1d211e4052c4b025c965eb569a50f5d4312b
milad-emami <milad.emami1025@gmail.com>
2022-12-19 15:49:41 +0330

    feat: add top program

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -2,12 +2,14 @@ import Banner from '../banner/Banner';
 import ExplorerCourses from '../explorer-courses/ExplorerCourses';
 import Partners from '../partners/Partners';
 import PopularSubjects from '../popular-subjects/PopularSubjects';
+import TopPrograms from '../top-programs/TopPrograms';
 
 const HomePage = () => (
   <main>
     <Banner />
     <PopularSubjects />
     <ExplorerCourses />
+    <TopPrograms />
     <Partners />
   </main>
 );
--- src/components/top-programs/program-card/ProgramCard.jsx
+++ src/components/top-programs/program-card/ProgramCard.jsx
@@ -1,59 +1,29 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable camelcase */
-// variables taken from algolia not in camelcase
 import React from 'react';
 
-import {
-  Button,
-  Card,
-} from '@edx/paragon';
+import { Button, Card } from '@edx/paragon';
 
 const ProgramCard = ({ info }) => {
   const {
-    title,
-    institution,
-    cover,
-    logo,
+    title, institution, cover, logo,
   } = info;
 
   return (
     <Card>
-      <Card.ImageCap
-        src={cover}
-        logoSrc={logo}
-        variant="top"
-        // src={buildLmsUrl(course.media.course_image.uri)}
-        alt=""
-      />
-      <div className="mt-4.5 px-4 d-flex flex-column program-card-container">
+      <Card.ImageCap src={cover} logoSrc={logo} variant="top" alt="" />
+      <div className="mt-4.5 px-4 d-flex flex-column">
         <h3 className="program-title">{title}</h3>
-        <a className="program-link" href="#institution">{institution}</a>
-
+        <a className="program-link mb-3" href="#institution">
+          {institution}
+        </a>
       </div>
-
-      <Card.Section>
-        <Button variant="outline-primary" size="sm">Degree</Button>
-
-        {/* <div className="d-flex flex-column mb-3">
-          <div className="d-flex flex-row align-items-center mb-2">
-            <Icon className="card-icon" src={Person} />
-            <a href="#instructor">{instructor}</a>
-          </div>
-          <div className="d-flex flex-row align-items-center mb-2">
-            <Icon className="card-icon" src={BookOpen} />
-            <span>{lessons}</span>
-          </div>
-          <div className="d-flex flex-row align-items-center mb-3.5">
-            <Icon className="card-icon" src={WatchFilled} />
-            <span>{ time}</span>
-          </div>
-          <span className="text-gray-500">$100 . Available Now</span>
-        </div> */}
-      </Card.Section>
-
       <Card.Footer>
-        <span className="program-desc">Professional Certificate (9 courses)</span>
-
+        <Button className="btn-program" variant="outline-primary" size="sm">
+          Degree
+        </Button>
+        <span className="program-desc mt-2.5">
+          Professional Certificate (9 courses)
+        </span>
       </Card.Footer>
     </Card>
   );
