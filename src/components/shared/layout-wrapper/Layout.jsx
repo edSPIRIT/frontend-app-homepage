deb163aae0b3c5d869dc56eb7c39d99571bafcc9
milad-emami <milad.emami1025@gmail.com>
2023-06-19 17:11:39 +0330

    fix: fix layout in mobile view

--- src/components/shared/layout-wrapper/Layout.jsx
+++ src/components/shared/layout-wrapper/Layout.jsx
@@ -1,31 +1,42 @@
 /* eslint-disable react/prop-types */
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Spinner } from '@edx/paragon';
+import { useLocation } from 'react-router-dom';
 import Header from '../header/Header';
 import FooterSection from '../footer/FooterSection';
 import SearchModal from './layout/SearchModal';
 
-const Layout = ({ children }) => (
-  <Suspense
-    fallback={(
-      <div className="d-flex justify-content-center align-items-center">
-        <Spinner
-          animation="border"
-          className="mie-3 "
-          screenReaderText="loading"
-        />
-      </div>
+const Layout = ({ children }) => {
+  const [hasPriceWrapper, setHasPriceWrapper] = useState(false);
+  const location = useLocation();
+  console.log('location', location, location.pathname.includes('/course'));
+  useEffect(() => {
+    setHasPriceWrapper(location.pathname.includes('/course'));
+  }, [location]);
+
+  return (
+    <Suspense
+      fallback={(
+        <div className="d-flex justify-content-center align-items-center">
+          <Spinner
+            animation="border"
+            className="mie-3 "
+            screenReaderText="loading"
+          />
+        </div>
     )}
-  >
-    <SearchModal />
-    <div className="layout-container">
-      <Header />
-      <main className="main-container">
-        {children}
-      </main>
-      <FooterSection />
-    </div>
-  </Suspense>
-);
+    >
+      <SearchModal />
+      <div className={`layout-container ${hasPriceWrapper ? 'has-price-wrapper' : ''}`}>
+        <Header />
+        <main className="main-container">
+          {children}
+        </main>
+        <FooterSection />
+      </div>
+    </Suspense>
+  );
+};
 
 export default Layout;
+  <div className="price-wrapper">price</div>;
