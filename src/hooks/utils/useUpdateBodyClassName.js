648a0f9066a7d1f9f317ae12aa568639058d6277
milad-emami <milad.emami1025@gmail.com>
2023-08-13 13:13:54 +0330

    fix: fix fa-ir issue

--- src/hooks/utils/useUpdateBodyClassName.js
+++ src/hooks/utils/useUpdateBodyClassName.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const useUpdateBodyClassName = () => {
   useEffect(() => {
     const body = document.querySelector('body');
-    if (getLocale() === 'fa') {
+    if (getLocale() === 'fa' || getLocale() === 'fa-ir') {
       body.className = 'lang_fa';
     } else if (getLocale() === 'ar') {
       body.className = 'lang_ar';
