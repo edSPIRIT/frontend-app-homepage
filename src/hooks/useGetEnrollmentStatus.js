25aade6d1a61cbdd7142da02edca7a021fed9243
milad-emami <milad.emami1025@gmail.com>
2023-09-04 11:45:15 +0330

    fix: prevent api for unauthenticated users

--- src/hooks/useGetEnrollmentStatus.js
+++ src/hooks/useGetEnrollmentStatus.js
@@ -1,8 +1,11 @@
 import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { AppContext } from '@edx/frontend-platform/react';
+import { useContext } from 'react';
 import { useQuery } from 'react-query';
 
 const useGetEnrollmentStatus = (courseId) => {
+  const { authenticatedUser } = useContext(AppContext);
   const fetchEnrollmentStatus = async (course_id) => {
     const { data, status } = await getAuthenticatedHttpClient().get(
       `${getConfig().LMS_BASE_URL}/api/enrollment/v1/enrollment/${course_id}`,
@@ -16,7 +19,7 @@ const useGetEnrollmentStatus = (courseId) => {
     ['enrollmentStatus', courseId],
     () => fetchEnrollmentStatus(courseId),
     {
-      enabled: !!courseId,
+      enabled: !!courseId && !!authenticatedUser,
     },
   );
   return {
