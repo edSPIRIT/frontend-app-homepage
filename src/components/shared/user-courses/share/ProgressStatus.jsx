d3e88e6f06b5076bcf63f2bda8a2fa10d111bfe3
milad-emami <milad.emami1025@gmail.com>
2023-09-11 10:24:43 +0330

    fix: first show pre reg in dashboard

--- src/components/shared/user-courses/share/ProgressStatus.jsx
+++ src/components/shared/user-courses/share/ProgressStatus.jsx
@@ -12,32 +12,6 @@ const ProgressStatus = ({
   isCourseNotStarted,
   preReqCourse,
 }) => {
-  if (courseCompleted) {
-    return (
-      <div className="d-flex align-items-center ">
-        <Icon className="check-circle-icon mr-2.5" src={CheckCircle} />
-        <span className="second-title">
-          <FormattedMessage
-            id="userCourseCard.completed.text"
-            defaultMessage="Well done! You completed this course"
-          />
-        </span>
-      </div>
-    );
-  }
-  if (isCourseNotStarted) {
-    return (
-      <div className="d-flex align-items-start">
-        <Icon className="mr-1 warning-icon" src={Warning} />
-        <span className="font-sm">
-          <FormattedMessage
-            id="courseInfo.starCourseDate.attention"
-            defaultMessage="Course coming soon! Stay tuned for the start date"
-          />
-        </span>
-      </div>
-    );
-  }
   if (preReqCourse) {
     return (
       <div className="d-flex align-items-start">
@@ -64,6 +38,33 @@ const ProgressStatus = ({
       </div>
     );
   }
+  if (courseCompleted) {
+    return (
+      <div className="d-flex align-items-center ">
+        <Icon className="check-circle-icon mr-2.5" src={CheckCircle} />
+        <span className="second-title">
+          <FormattedMessage
+            id="userCourseCard.completed.text"
+            defaultMessage="Well done! You completed this course"
+          />
+        </span>
+      </div>
+    );
+  }
+  if (isCourseNotStarted) {
+    return (
+      <div className="d-flex align-items-start">
+        <Icon className="mr-1 warning-icon" src={Warning} />
+        <span className="font-sm">
+          <FormattedMessage
+            id="courseInfo.starCourseDate.attention"
+            defaultMessage="Course coming soon! Stay tuned for the start date"
+          />
+        </span>
+      </div>
+    );
+  }
+
   return <ProgressBar now={calcProgress()} label={`${calcProgress()}%`} />;
 };
 
