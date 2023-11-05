40d02dd254536bc2e162d819852ef81e21e1eef3
milad-emami <milad.emami1025@gmail.com>
2023-11-05 21:46:25 +0330

    fix: fix transacted about course info

--- src/components/course-info-page/CoursePage/share/AboutCourse.jsx
+++ src/components/course-info-page/CoursePage/share/AboutCourse.jsx
@@ -1,15 +1,38 @@
 /* eslint-disable react/prop-types */
 import { FormattedMessage, injectIntl } from '@edx/frontend-platform/i18n';
-import { Skeleton, useMediaQuery } from '@edx/paragon';
-import ShowMoreText from 'react-show-more-text';
+import { Button, Skeleton, useMediaQuery } from '@edx/paragon';
 import ReactPlayer from 'react-player';
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 import messages from '../../../../messages';
 
 const AboutCourse = ({
   courseVideoUrl, aboutCourse, loading, intl,
 }) => {
   const isMobile = useMediaQuery({ maxWidth: '768px' });
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
+  const contentRef = useRef(null);
+  useEffect(() => {
+    if (contentRef.current) {
+      const height = contentRef.current.offsetHeight;
+      if (height > 99) {
+        setShow(true);
+      }
+    }
+  }, [aboutCourse]);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const displayButtonContent = () => {
+    if (!show) {
+      return null;
+    }
+    if (isOpen) {
+      return intl.formatMessage(messages['showLess.text']);
+    }
+    return intl.formatMessage(messages['showMore.text']);
+  };
   return (
     <div className="about-wrapper" id="about-course">
       <h2 className="mb-3">
@@ -22,20 +45,26 @@ const AboutCourse = ({
         <Skeleton count={4} height={24} />
       ) : (
         <>
-          <ShowMoreText
-            lines={3}
-            more={intl.formatMessage(messages['showMore.text'])}
-            less={intl.formatMessage(messages['showLess.text'])}
-            className="content-css"
-            anchorClass="show-more-less-clickable"
-            expanded={false}
-            truncatedEndingComponent="... "
+          <div
+            className={classNames('', {
+              'about-content-wrapper': show && !isOpen,
+            })}
+            ref={contentRef}
+            style={{
+              maxHeight: isOpen ? 'none' : '100px',
+              overflow: 'hidden',
+              background:
+                'linear-gradient(to bottom, #ffffff 0%, transparent 100%)',
+            }}
           >
             <div
               className="mb-2"
               dangerouslySetInnerHTML={{ __html: aboutCourse }}
             />
-          </ShowMoreText>
+          </div>
+          <Button variant="tertiary" onClick={toggle}>
+            {displayButtonContent()}
+          </Button>
           {!isMobile && courseVideoUrl && (
             <div className="mt-4 player-wrapper">
               <ReactPlayer
