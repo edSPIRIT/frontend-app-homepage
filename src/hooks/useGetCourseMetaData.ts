70800bfc9400bec427012b949659ca27b7458d41
Milad Emami <74170652+milad-emami@users.noreply.github.com>
2023-12-02 22:59:47 +0330

    fix: remove comment (#6)

--- src/hooks/useGetCourseMetaData.ts
+++ src/hooks/useGetCourseMetaData.ts
@@ -1,9 +1,8 @@
 /* eslint-disable consistent-return */
-import { getConfig } from '@edx/frontend-platform';
-// import axios from 'axios';
-import { useQuery } from 'react-query';
-import { useHistory } from 'react-router';
-import { ICourseMetaData } from './interfaces/APIResponsesInterfaces';
+import { getConfig } from "@edx/frontend-platform";
+import { useQuery } from "react-query";
+import { useHistory } from "react-router";
+import { ICourseMetaData } from "./interfaces/APIResponsesInterfaces";
 
 const useGetCourseMetaData = (courseId) => {
   const history = useHistory();
@@ -11,49 +10,24 @@ const useGetCourseMetaData = (courseId) => {
   const fetchCourseMetaData = async ({ queryKey }) => {
     const id = queryKey[1];
     const apiRes = await fetch(
-      `${getConfig().LMS_BASE_URL}/admin-console/api/course-metadata/${id}/`,
-      // {
-      //   method: 'GET',
-      //   cache: 'no-store',
-      // },
+      `${getConfig().LMS_BASE_URL}/admin-console/api/course-metadata/${id}/`
     );
     if (apiRes.status === 404) {
-      history.push('/404');
+      history.push("/404");
     }
     if (!apiRes.ok) {
-      throw new Error('fetch not ok');
+      throw new Error("fetch not ok");
     }
 
     return apiRes.json();
   };
-  // const fetchCourseMetaData = async ({ queryKey }) => {
-  //   const id = queryKey[1];
-  //   try {
-  //     const apiRes = await axios.get(
-  //       `${getConfig().LMS_BASE_URL}/admin-console/api/course-metadata/${id}/`,
-  //       // {
-  //       //   headers: {
-  //       //     'Cache-Control': 'no-cache',
-  //       //     Pragma: 'no-cache',
-  //       //     Expires: '0',
-  //       //   },
-  //       // },
-  //     );
-  //     return apiRes.data;
-  //   } catch (err) {
-  //     console.error(err);
-  //     if (err.response.status === 404) {
-  //       history.push('/404');
-  //     }
-  //   }
-  // };
   const { data, isLoading } = useQuery<ICourseMetaData>(
-    ['CourseMetaData', courseId],
+    ["CourseMetaData", courseId],
     fetchCourseMetaData,
     {
       enabled: !!courseId,
       staleTime: 2 * (60 * 1000),
-    },
+    }
   );
 
   return {
