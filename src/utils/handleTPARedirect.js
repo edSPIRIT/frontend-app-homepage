76de9998a5d0b1ce19946f50c67261a25e1a7842
Khaled <info@khaledr.ir>
2023-12-31 11:24:55 +0330

    fix: reverted changes over handleTPARedirect.js

--- src/utils/handleTPARedirect.js
+++ src/utils/handleTPARedirect.js
@@ -6,8 +6,8 @@ const handleTPARedirect = (TPAQueryparam) => {
 
   // Set the URL parameters for the redirect back to your home page after login
   const redirectParams = `next=${encodeURIComponent(
-    document.referrer,
-  )}&origin=${window.location.origin}`;
+    window.location.href,
+  )}&origin=${encodeURIComponent(window.location.origin)}`;
 
   // Redirect the user to the login page with the redirect parameters
   window.location.href = `${loginUrl}${TPAQueryparam}&${redirectParams}`;
